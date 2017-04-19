26a4980de77806fe103c50fc0638ecf78c5a4d44
Charlyzzz <erwincdl@gmail.com>
2017-04-19 16:43:56 -0300

    Restrict params validation only on create

--- web/controllers/mobile_controller.ex
+++ web/controllers/mobile_controller.ex
@@ -2,7 +2,7 @@ defmodule EstacionappServer.MobileController do
   use EstacionappServer.Web, :controller
   alias EstacionappServer.Driver
 
-  plug EstacionappServer.Plugs.Params, Driver
+  plug EstacionappServer.Plugs.Params, Driver when action in [:create]
 
   def create(_conn, _params) do
 
