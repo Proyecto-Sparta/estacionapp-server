71ac967b2b06cce1930784e1df17d511feb7cc05
Charlyzzz <erwincdl@gmail.com>
2017-04-30 20:36:35 -0300

    garage controller documentation

--- web/controllers/garage_controller.ex
+++ web/controllers/garage_controller.ex
@@ -1,8 +1,21 @@
 defmodule EstacionappServer.GarageController do
-  use EstacionappServer.Web, :controller
+  @moduledoc """
+  This is the controller for all API calls related with garages.
+  """
 
+  use EstacionappServer.Web, :controller
   alias EstacionappServer.{Garage, Repo}
 
+  @doc """
+  Inserts a new Garage. 
+  Returns the inserted garage id or a hash with changeset errors.
+  Params:{
+    location: [lat, long],
+    username: string,
+    email: string,
+    garage_name: string
+  }
+  """
   def create(conn, params) do
     params = Map.update(params, "location", nil, &make_coordinates/1)
     %Garage{}
@@ -22,12 +35,29 @@ defmodule EstacionappServer.GarageController do
       end
   end
 
+  @doc """
+  Authenticates a garage. 
+  Returns the jwt token inside authorization header.
+  Params:{
+    username: string
+  }
+  """
   def login(conn, params) do
     Repo.get_by(Garage, username: Map.get(params, "username", ""))
       |> case do
         nil -> resp_unauthorized(conn, "invalid login credentials")
         garage -> authenticate(garage, conn)
       end
+  end
+
+  defp authenticate(garage, conn) do
+    if Guardian.Plug.authenticated?(conn), do: Guardian.Plug.sign_out(conn)
+    new_conn = Guardian.Plug.api_sign_in(conn, garage)
+    jwt = Guardian.Plug.current_token(new_conn)
+    new_conn
+      |> put_resp_header("authorization", "Bearer #{jwt}")
+      |> put_status(:accepted)
+      |> json(%{status: "logged in"})
   end
 
   def unauthenticated(conn, _params), do: resp_unauthorized(conn, "login needed")
@@ -42,15 +72,5 @@ defmodule EstacionappServer.GarageController do
     conn
       |> put_status(:unauthorized)
       |> json(%{status: message})
-  end
-
-  defp authenticate(garage, conn) do
-    if Guardian.Plug.authenticated?(conn), do: Guardian.Plug.sign_out(conn)
-    new_conn = Guardian.Plug.api_sign_in(conn, garage)
-    jwt = Guardian.Plug.current_token(new_conn)
-    new_conn
-      |> put_resp_header("authorization", "Bearer #{jwt}")
-      |> put_status(:accepted)
-      |> json(%{status: "logged in"})
   end
 end
