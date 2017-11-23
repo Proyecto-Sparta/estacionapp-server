1520436c12a71b0c4f7b747889cd301901987bb3
Erwin Debusschere <erwincdl@gmail.com>
2017-11-23 17:41:09 -0300

    Updated seeds to match demo planning

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -51,11 +51,10 @@ end
   |> Stream.with_index
   |> Enum.map(&Seeds.insert_amenity/1)
 
-Seeds.insert_driver("jnoziglia", "Julian Noziglia", %{type: "car", plate: "AG 759 LH"})
-Seeds.insert_driver("fedescarpa", "Federico Scarpa", %{type: "pickup", plate: "OUI 915"})
+#Datos de generales
+Seeds.insert_driver("jnoziglia", "Julian Noziglia", %{type: "car", plate: "OUI 915"})
 Seeds.insert_driver("erwincdl", "Erwin Debusschere", %{type: "bike", plate: "Shimano"})
 Seeds.insert_driver("MarianoP", "Mariano Paolucci", %{type: "bike", plate: "Shimano Roja"})
-
 
 #Square shape
 general_outline = [%{x: 25, y: 18 }, %{x: 855, y: 13}, %{x: 858, y: 576}, %{x: 45, y: 579}]
@@ -70,17 +69,16 @@ Seeds.insert_garage("Apart Car Monserrat", [-34.613369, -58.383643], %{car: 44, 
 Seeds.insert_garage("Apart Car Madero Marie", [-34.620637, -58.366168], %{car: 32, bike: 22, pickup: 80}, general_outline, [1,2,3,7,8])
 
 
-
 general_parking_spaces = [
-  %{ x: 100, y: 100, width: 75, height: 100, occupied: true , shape: "square", angle: 0 },
-  %{ x: 100, y: 250, width: 75, height: 100, occupied: true , shape: "square", angle: 0 },
-  %{ x: 100, y: 400, width: 75, height: 100, occupied: true , shape: "square", angle: 0 },
-  %{ x: 450, y: 100, width: 75, height: 100, occupied: true , shape: "square", angle: 0 },
-  %{ x: 450, y: 250, width: 75, height: 100, occupied: true , shape: "square", angle: 0 },
-  %{ x: 450, y: 400, width: 75, height: 100, occupied: true , shape: "square", angle: 0 },
-  %{ x: 550, y: 100, width: 75, height: 100, occupied: false, shape: "square", angle: 0 },
-  %{ x: 550, y: 250, width: 75, height: 100, occupied: false, shape: "square", angle: 0 },
-  %{ x: 550, y: 400, width: 75, height: 100, occupied: false , shape: "square", angle: 0 }
+  %{ x: 100, y: 100, width: 60, height: 60, occupied: true , shape: "square", angle: 0 },
+  %{ x: 100, y: 250, width: 60, height: 60, occupied: true , shape: "square", angle: 0 },
+  %{ x: 100, y: 400, width: 60, height: 60, occupied: true , shape: "square", angle: 0 },
+  %{ x: 450, y: 100, width: 60, height: 60, occupied: true , shape: "square", angle: 0 },
+  %{ x: 450, y: 250, width: 60, height: 60, occupied: true , shape: "square", angle: 0 },
+  %{ x: 450, y: 400, width: 60, height: 60, occupied: true , shape: "square", angle: 0 },
+  %{ x: 550, y: 100, width: 60, height: 60, occupied: false, shape: "square", angle: 0 },
+  %{ x: 550, y: 250, width: 60, height: 60, occupied: false, shape: "square", angle: 0 },
+  %{ x: 550, y: 400, width: 60, height: 60, occupied: false , shape: "square", angle: 0 }
 ]
 
 
@@ -92,3 +90,35 @@ Seeds.insert_garage_layout(5, 1, general_parking_spaces)
 Seeds.insert_garage_layout(6, 1, general_parking_spaces)
 Seeds.insert_garage_layout(7, 1, general_parking_spaces)
 Seeds.insert_garage_layout(8, 1, general_parking_spaces)
+
+#Datos para demo
+##Conductor
+Seeds.insert_driver("fedescarpa", "Federico Scarpa", %{type: "pickup", plate: "AA 509 GD"})
+
+#G1
+Seeds.insert_garage("UTN parking", [-34.597890, -58.420338], %{bike: 0, car: 80, pickup: 120}, general_outline, [2, 3, 7])
+#G2
+Seeds.insert_garage("DISI parking", [-34.599137, -58.420287], %{bike: 0, car: 50, pickup: 90}, general_outline, [2, 3, 4, 5, 7, 8])
+#G3
+Seeds.insert_garage("Aparcamientos PROJECT", [-34.600359, -58.422099], %{bike: 15, car: 30, pickup: 80}, general_outline, [1, 2, 3, 8])
+
+##Garage
+#Franco
+Seeds.insert_garage("Mumuki aparcaderos", [-34.603168, -58.421574], %{bike: 10, car: 40, pickup: 70}, general_outline, [1, 2, 3, 6, 7])
+
+first_floor = [
+  %{ x: 183, y: 123, width: 40, height: 40, occupied: true, shape: "square", angle: 0 },
+  %{ x: 293, y: 124, width: 40, height: 40, occupied: true, shape: "square", angle: 0 },
+  %{ x: 386, y: 124, width: 40, height: 40, occupied: true, shape: "square", angle: 0 }
+]
+
+second_floor = [
+  %{ x: 271, y: 210, width: 40, height: 40, occupied: true, shape: "square", angle: 0 },
+  %{ x: 144, y: 215, width: 40, height: 40, occupied: false, shape: "square", angle: 0 },
+  %{ x: 401, y: 53, width: 40, height: 40, occupied: false, shape: "square", angle: 0 },
+  %{ x: 272, y: 53, width: 40, height: 40, occupied: false, shape: "square", angle: 0 },
+  %{ x: 147, y: 60, width: 40, height: 40, occupied: false, shape: "square", angle: 0 }
+]
+
+Seeds.insert_garage_layout(8, 1, first_floor)
+Seeds.insert_garage_layout(8, 2, second_floor)
